c4304283fe787ad37a68ee5802f2ce4119c33923
rAel-Ael <alediru9267@gmail.com>
2026-05-21 15:52:41 +0200

    creato server e le robe da vedere

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 import express from 'express';
 
+const app = express();
+
+app.get('/', (request, response) => {
+    response.type (<h1>Server del mio blog</h1>   
+)
+
+
+});
+
+app.get('/bacheca', (request, response) => {
+    response.json(
+        
+        [
+            'post 1',
+            'post 2',
+            'post 3'
+        ]);
+    
+
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Server in ascolto sulla porta 3000');
+});
+
+
